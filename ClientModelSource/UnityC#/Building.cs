84c9233d46c8fd7b939a3fda3d0569b7a585b3ee
Петр Цап <petrtsap@gmail.com>
2016-04-03 14:30:16 +1000

    00006 Уточнения модели данных

--- ClientModelSource/UnityC#/Building.cs
+++ ClientModelSource/UnityC#/Building.cs
@@ -1,38 +1,29 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using StrategyMagic.GameArchitect;
 
-
-namespace StrategyMagic.GameArchitect {  
-	[Serializable]
-	public class Building : CatalogObject {
-		// [Serializable]
-		// public class Price :  object 
-		// {
-		// 	public string Resource;
-		// 	public int Amount = 0;
-		// 	public Price(string Resource, int Amount)
-		// 	{
-		// 		this.Resource = Resource;
-		// 		this.Amount = Amount;
-		// 	}
-		// }
-
-		// public string GameDescr;
-		public int Level = 0;
-		// public List<Price> cost = new List<Price>();
-		public Buildings (string GameDescr, int Level, List<Price> cost)
-		{
-		// 	this.GameDescr = GameDescr;
-			this.Level = Level;
-		//	this.cost = cost;
-		}
-
-		public static override Buildings CreateFromJson(string JsonString)
-		{
-			return JsonUtility.FromJson<Buildings> (JsonString);
-		}
-
-	}//public class Buildings
+namespace StrategyMagic.GameArchitect
+{
+    [Serializable]
+    public class Building : CatalogObject
+    {
+        // [Serializable]
+        // public class Price :  object 
+        // {
+        // 	public string Resource;
+        // 	public int Amount = 0;
+        // 	public Price(string Resource, int Amount)
+        // 	{
+        // 		this.Resource = Resource;
+        // 		this.Amount = Amount;
+        // 	}
+        // }
+        public int Level = 0;
+        // public List<Price> cost = new List<Price>();
+        public Building(string CurCode, string CurDescr,  int Level)
+        {
+            Code = CurCode;
+            Description = CurDescr;
+            this.Level = Level;
+            //	this.cost = cost;
+        }
+    }//public class Building
+}
